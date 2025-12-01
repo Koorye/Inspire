be6ea5131f09c317c54fdb7aa094f51ab0de5739
Koorye <a1311586225@gmail.com>
2025-12-01 17:28:26 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/proj/Inspire/',
+  base:'/Inspire/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
