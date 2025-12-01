cef20a990987dd31aee68694106e5d9fdd10539b
Koorye <a1311586225@gmail.com>
2025-12-01 16:35:24 +0800

    update

---
+++ src/components/sections/ExperimentCalvin.vue
@@ -0,0 +1,31 @@
+<script setup>
+import experiment from '../mds/experiment_calvin.mdx';
+</script>
+
+<template>
+  <div v-animate-onscroll="'animated fadeInUp'">
+    <el-row justify="center">
+        <el-col :xs="28" :sm="24" :md="20" :lg="16" :xl="12">
+            <el-image src="./calvin_results.png" class="caption-img" fit="cover" />
+            <experiment class="caption-text" />
+        </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<style scoped>
+
+.title {
+  margin-bottom: 20px;
+}
+
+.caption-img {
+    width: 100%;
+}
+
+.caption-text {
+    margin: 20px 0;
+}
+
+</style>
